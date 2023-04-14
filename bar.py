55fef8459cecddad2dfa5000d68bb953a687db15
your-name <katongole.roy100@gmail.com>
2023-04-14 16:52:41 +0300

    bar manager

--- bar.py
+++ bar.py
@@ -3,30 +3,36 @@ inventory = {}
 print("Welcome to the alcohol bar management program!\n")
 
 while True:
-    print("Choose an option:\n1. Add a bottle to the bar\n2. Remove a bottle from the bar\n3. Purchase a bottle from the bar\n4. Print the current inventory of the bar\n5. Quit the program")
+    print("Choose an option:\n1. Add bottles to the bar\n2. Remove a bottle from the bar\n3. Purchase a bottle from the bar\n4. Print the current inventory of the bar\n5. Quit the program")
     
     choice = int(input("\nEnter your choice: "))
 
-#Add a bottle to the bar
+#Add bottles to the bar
 
     if choice == 1:
-        name = input("\nEnter the name of the bottle: ")
-        quantity = int(input("Enter the quantity of the bottle: "))
+        num_bottles = int(input("\nEnter the number of bottles you want to add: "))
         
-        if name in inventory:
-            inventory[name] += quantity
-        else:
-            inventory[name] = quantity
+        for i in range(num_bottles):
+            name = input(f"\nEnter the name of bottle #{i+1}: ")
+            quantity = int(input("Enter the quantity of the bottle: "))
             
-        print(f"\n{name} has been added to the bar.\n")
+            if name in inventory:
+                inventory[name] += quantity
+            else:
+                inventory[name] = quantity
+                
+            print(f"\n{name} has been added to the bar.\n")
 
 #Remove a bottle from the bar  
 
     elif choice == 2:
         name = input("\nEnter the name of the bottle you want to remove: ")
-        
+
         if name in inventory:
-            del inventory[name]
+            quantity = int(input("Enter the quantity you want to remove: "))
+
+            if inventory[name] >= quantity:
+                inventory[name] -= quantity
             print(f"\n{name} has been removed from the bar.\n")
         else:
             print(f"\n{name} is not in the bar.\n")
